054067fbf1d3559658c76eb65b67fec17b0fddb6
David Stevenson <dsboulder@gmail.com>
2013-01-10 08:08:59 -0800

    Styling fixes

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -1,5 +1,8 @@
 $(function() {
   $('form .project .project_enabled').click(function() {
-    $(this).closest('.project').find("section.labels").toggle($(this).is('checked'));
+    var $this = $(this);
+    _(function() {
+      $this.closest('.project').find("section.labels").toggle($this.is(':checked'));
+    }).defer();
   });
 });
